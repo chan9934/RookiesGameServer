01212a555d5ade636164b9b8ddbdf4417f8f5d82
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-10-10 09:06:10 +0900

    GameServer 공부

---
+++ ServerCore/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ServerCore
+{
+    internal class Program
+    {
+        static void MainThread(object state)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine("HelloThread");
+            }   
+            
+        }
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 4; ++i)
+            {
+                ThreadPool.QueueUserWorkItem((obj)=> { while (true) { } });
+            }
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+            
+            while(true)
+            {
+
+            }
+        }
+    }
+}
