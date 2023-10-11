77eee0cc43c866a08ed912791720ed694868a853
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-10-12 08:59:30 +0900

    Study

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,46 +2,31 @@
 {
     internal class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
-
+        static int threadNumber = 0;
         static void Thread_1()
         {
-            y = 1;
-            Thread.MemoryBarrier();
-            r1 = x;
+            for(int i = 0; i < 1000000; ++i)
+            {
+               Interlocked.Increment(ref threadNumber);
+            }
         }
         static void Thread_2()
         {
-            x = 1;
-            Thread.MemoryBarrier();
-            r2 = y;
+            for(int i = 0; i < 1000000;  i++)
+            {
+                Interlocked.Decrement(ref threadNumber);
+            }
         }
         static void Main(string[] args)
         {
-            int count = 0;
-            while(true)
-            {
-                count++;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                x = y = r1 = r2 = 0;
+            Task.WaitAll(t1, t2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if(r1 == 0 && r2 == 0)
-                {
-                    break;
-                }
-            }
-            Console.WriteLine($"{count}번만에 끝냄");
-
+            Console.WriteLine($"{threadNumber}");
         }
     }
 }
