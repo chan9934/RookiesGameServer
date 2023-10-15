33cb067c9fcefdf4622570db9704f1e7ad6849d1
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-10-16 08:51:31 +0900

    DeadLockStudy

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,31 +2,65 @@
 {
     internal class Program
     {
-        static int threadNumber = 0;
-        static void Thread_1()
+        class SessionManager
         {
-            for(int i = 0; i < 1000000; ++i)
+            static object _lock = new object();
+
+            public static void Test()
             {
-               Interlocked.Increment(ref threadNumber);
+                lock(_lock)
+                {
+                    UserManager.TestSession();
+                }
+            }
+            public static void TestSession()
+            {
+                lock(_lock) { }
             }
         }
-        static void Thread_2()
+        class UserManager
         {
-            for(int i = 0; i < 1000000;  i++)
+            static object _lock = new object();
+
+            public static void Test()
             {
-                Interlocked.Decrement(ref threadNumber);
+                lock(_lock)
+                {
+                    SessionManager.TestSession();
+                }
+            }
+            public static void TestSession()
+            {
+                lock(_lock) { }
+            }
+        }
+
+        static void Thread1()
+        {
+            for(int i = 0; i < 100; ++i)
+            {
+                SessionManager.Test();
+            }
+        }
+        static void Thread2()
+        {
+            for(int i = 0; i < 100; ++i)
+            {
+                UserManager.Test();
             }
         }
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t1 = new Task(Thread1);
+            Task t2 = new Task(Thread2);
             t1.Start();
+            Thread.Sleep(100);
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine($"{threadNumber}");
+            Console.WriteLine("종료");
+
         }
     }
 }
