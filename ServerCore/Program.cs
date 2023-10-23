803a814fd3f9c114fadfbba792b1c5546cb66b01
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-10-24 08:16:57 +0900

    TLS 공부

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,38 +2,27 @@
 {
     internal class Program
     {
-        static volatile int _count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> threadName = new ThreadLocal<string>(() => { return $"Thread Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        static void whoAmI()
+        {
+            bool Result = threadName.IsValueCreated;
+            if(Result)
+            {
+
+                Console.WriteLine($"{ threadName.Value} Repeated");
+            }
+            else
+            {
+
+                Console.WriteLine(threadName.Value);
+            }
+        }
         static void Main(string[] args)
         {
-            Task t1 = new Task(
-                delegate ()
-                {
-                    for (int i = 0; i < 100000; ++i)
-                    {
-                        _lock.WriteLock();
-                        _lock.WriteLock();
-                        _count++;
-                        _lock.WriteUnLock();
-                        _lock.WriteUnLock();
-                    }
-                }
-                );
-            Task t2 = new Task(
-                delegate ()
-                {
-                    for (int i = 0; i < 100000; ++i)
-                    {
-                        _lock.WriteLock();
-                        _count--;
-                        _lock.WriteUnLock();
-                    }
-                }
-                );
-            t1.Start();
-            t2.Start();
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(_count);
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(whoAmI, whoAmI, whoAmI, whoAmI, whoAmI);
         }
     }
 }
