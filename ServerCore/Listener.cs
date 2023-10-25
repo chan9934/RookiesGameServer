8bcefeb0168fba04638d3f73efca07a615cb388c
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-10-25 23:11:13 +0900

    [add]Listener class 추가

---
+++ ServerCore/Listener.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            _listenSocket.Bind(endPoint);
+            _listenSocket.Listen(10);
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        public void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+            bool pending = _listenSocket.AcceptAsync(args);
+            if(pending == false)
+            {
+                OnAcceptCompleted(null, args);
+            }
+
+        }
+        void OnAcceptCompleted(Object Sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            RegisterAccept(args);
+        }
+    }
+}
